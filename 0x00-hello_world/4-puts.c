363d089bf27425d963741a53771160242948d2d8
finlaymatunda <matundafinlay@gmail.com>
2023-02-16 07:05:23 -0800

    4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
+
 /**
-*Header - Declare the main
-*Return 0 when successful
+*main - Entry point
+*
+*Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+			return (0);
 }
 
