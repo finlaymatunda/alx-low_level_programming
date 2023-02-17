f0d62bfc37d1587e207795f64e2c3df0d04b3a6e
finlaymatunda <matundafinlay@gmail.com>
2023-02-17 12:49:11 -0800

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int digit1, digit2;
-
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -20,13 +19,11 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
