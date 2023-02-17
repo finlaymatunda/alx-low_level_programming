7ded228edd7b5a7639cff927086a508121b7337b
finlaymatunda <matundafinlay@gmail.com>
2023-02-17 09:24:40 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,28 +4,23 @@
 
 /**
  * Main header - Declare header
- * Return 0 (When successful)
+ * Return 0 (When successfuL
  */
-int main (void)
-{       
-        int n;
-        $rand(time(0));
-        n= rand() - RAND-MAX/2;
-        
-      if (n > 0){
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 		printf("%d is positive\n", n);
-      }
-	
-      else if (n < 0){
-
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	
-      }
-      else{
+	else
 		printf("%d is zero\n", n);
-	}
+
 	return (0);
-
-
 }
 
