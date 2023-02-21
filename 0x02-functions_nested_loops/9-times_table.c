bb8238973ef098b7536275fd73168c033e2d1b17
finlaymatunda <matundafinlay@gmail.com>
2023-02-21 06:58:17 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,26 @@
 
 void times_table(void)
 {
-	int i, j, k;
+	int a, b, c;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
+		_putchar('0');
 
-		for (j = 0; j <= 9; j++)
+		for (b = 1; b <= 9; b++)
 		{
-			k = i * j;
+			_putchar(',');
+			_putchar(' ');
 
-			if (k <= 9)
-			{
+			c = a * b;
+
+			if (c <= 9)
 				_putchar(' ');
-			}
 			else
-			{
-				_putchar((k / 10) + '0');
-			}
-			_putchar((k % 10) + '0');
+				_putchar((c / 10) + '0');
 
-			if (j != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar((c % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
 }
