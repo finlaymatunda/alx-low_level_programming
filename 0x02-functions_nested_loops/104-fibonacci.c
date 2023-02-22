3cfdc5e842c3c500caaecac707ce6b67c12cc1ee
finlaymatunda <matundafinlay@gmail.com>
2023-02-22 07:35:52 -0800

    Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,52 +1,38 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
-  * main- prints the first 98fibonaccinumbers   starting from 1,2;
-  *
-  * Result: Return 0;
-  */
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
+ */
 int main(void)
 {
+	unsigned long int f1 = 0, b1 = 1, f2 = 0, b2 = 2;
+	unsigned long int hold1, hold2, hold3;
 	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
 
-	for (count = 0; count < 92; count++)
+	printf("%lu, %lu, ", b1, b2);
+	for (count = 2; count < 98; count++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		if (b1 + b2 > LARGEST || f2 > 0 || f1 > 0)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			hold1 = (b1 + b2) / LARGEST;
+			hold2 = (b1 + b2) % LARGEST;
+			hold3 = f1 + f2 + hold1;
+			f1 = f2, f2 = hold3;
+			b1 = b2, b2 = hold2;
+			printf("%lu%010lu", f2, b2);
 		}
-
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
+		else
+		{
+			hold2 = b1 + b2;
+			b1 = b2, b2 = hold2;
+			printf("%lu", b2);
+		}
+		if (count != 97)
 			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
 	}
 	printf("\n");
-
-
 	return (0);
 }
