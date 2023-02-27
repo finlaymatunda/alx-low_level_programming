7ec0289961a426c152383a9b5b985a0a2d6f2fd5
finlaymatunda <matundafinlay@gmail.com>
2023-02-27 05:41:47 -0800

    puts task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
- */
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: the string
+**/
 
 void _puts(char *str)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,9 +3,11 @@
 
 /**
  * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
+ *
+ * Desc: header file
+**/
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,5 +19,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-
-#endif
+#endif /* MAIN_H  */
