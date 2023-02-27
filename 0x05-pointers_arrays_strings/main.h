2f8b26aa6a6f7b7669cf04e6c7f108d053d1f969
finlaymatunda <matundafinlay@gmail.com>
2023-02-27 04:08:41 -0800

    swap values

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,15 @@
  *       used in the 0x05-pointers_arrays_strings directory.
  */
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
 
 #endif
