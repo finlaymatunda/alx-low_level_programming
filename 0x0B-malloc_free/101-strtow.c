2793da1a30ef2e2fce009b6703bb590ea2a5b858
finlaymatunda <matundafinlay@gmail.com>
2023-03-14 07:36:18 -0700

    Redone task on malloc

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strtow - Main Entry
+ * Words are separated by spaces
+ *
+ * return NULL - if fail
+ */
+char **strtow(char *str);
+{
+	return (str);
+}
