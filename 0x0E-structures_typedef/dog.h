b053819a4dc25ef9a55d2bc2702c85febfc0e971
finlaymatunda <matundafinlay@gmail.com>
2023-03-20 05:39:41 -0700

    Pooopy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
-#ifndef DOG_H'
+#ifndef DOG_H
 #define DOG_H
+/**
+ * struct Dog - Dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Desc: header file
+ */
 
-typedef struct{
+typedef struct Dog
+{
 	char *name;
 	float age;
 	char *owner;
-}dog;
+}
+
 #endif
