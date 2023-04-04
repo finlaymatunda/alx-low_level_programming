b4a5f3094c8880b6a517ae3aba6c6c13608c79f1
finlaymatunda <matundafinlay@gmail.com>
2023-04-04 03:38:04 -0700

    Task 4 done

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -15,7 +15,7 @@ void free_listint2(listint_t *head)
         while (head != NULL)
         {
                head = linkednode
-	       	head = head->next;
+	       	linkednode = head->next;
                 free(linkednode);
         }
 }
