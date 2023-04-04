d15670dbe226782aa0cf6e60fc54cfbac715db06
finlaymatunda <matundafinlay@gmail.com>
2023-04-04 02:41:09 -0700

    Task threeattempt

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,32 +2,40 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * add_nodeint_end- adds a new node at the end of a listint_t list
- * @head: head of a list
- * @n: n element
- * Return: the address of the new element, or NULL if it failed
+ * add_nodeint_end - add a new node at the end of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
  */
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *node_new, *node_last;
+	listint_t *newnode;
+	listint_t *prevnode;
 
-	node_new = malloc(sizeof(listint_t));
-	if (node_new == NULL)
+	(void)prevnode;
+
+	newnode = malloc(sizeof(listint_t));
+
+	while (newnode == NULL){
 		return (NULL);
-
-	node_new->n = n;
-	node_new->next = NULL;
-
+	}
+	newnode->n = n;
+	newnode->next = NULL;
+	prevnode = *head;
 	if (*head == NULL)
-		*head = node_new;
-
+	{
+		*head = newnode;
+	}
 	else
 	{
-		node_last = *head;
-		while (node_last->next != NULL)
-			node_last = node_last->next;
-		node_last->next = node_new;
+		while (prevnode->next != NULL)
+		{
+			prevnode = prevnode->next;
+		}
+		prevnode->next = newnode;
 	}
 
 	return (*head);
